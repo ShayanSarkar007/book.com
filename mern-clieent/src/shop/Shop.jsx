06d6f13acd0c8b426b388231ee08193d6672a95e
ShayanSarkar007 <sarkar.sudhan9934@gmail.com>
2024-11-10 02:58:48 +0530

    Update Shop.jsx

--- mern-clieent/src/shop/Shop.jsx
+++ mern-clieent/src/shop/Shop.jsx
@@ -5,7 +5,7 @@ const Shop = () => {
   const [books, setBooks] = useState([]);
 
   useEffect( () => {
-    fetch("http://localhost:5000/all-books").then(res => res.json()).then(data => setBooks(data));
+    fetch("https://storylane-com.onrender.com").then(res => res.json()).then(data => setBooks(data));
       
     
   },[])
